006a0a29e198613c3623480f3bf7d9ee651fd7ec
Richard Routley <richardroutley@gmail.com>
2025-12-06 12:02:01 +0000

    Day 6 Part 2

--- Day6/Puzzle6.cs
+++ Day6/Puzzle6.cs
@@ -1,4 +1,4 @@
-// #define Sample
+//#define Sample
 
 public class Puzzle6 : IPuzzle
 {
@@ -11,7 +11,6 @@ public class Puzzle6 : IPuzzle
         var lines = File.ReadAllLines(input.FullName);
 #endif
         long result = 0;
-
 
         var problems = new int[lines.Length - 1][];
 
@@ -44,11 +43,55 @@ public class Puzzle6 : IPuzzle
             result += total;
         }
 
-
-
-
         System.Console.WriteLine("Part 1 = {0}", result);
 
+
+        result = 0;
+
+        // Use the position of the operations in the last line
+        // to determine where the columns are
+        List<(char opeator, int start, int end)> columns = [];
+        string lastLine = lines[^1];
+        int last = lastLine.Length + 1;
+        for (int i = lastLine.Length - 1; i >= 0; i--)
+        {
+            var c = lastLine[i];
+            if (c != ' ')
+            {
+                columns.Add((c, i, last - 1));
+                last = i;
+            }
+        }
+
+        foreach (var column in columns)
+        {
+            long total = 0;
+            System.Console.WriteLine(column);
+            for (int j = column.start; j < column.end; j++)
+            {
+                int value = 0;
+                for (int i = 0; i < lines.Length - 1; i++)
+                {
+                    if (char.IsAsciiDigit(lines[i][j]))
+                        value = value * 10 + lines[i][j] - '0';
+                }
+
+                if (j == column.start)
+                {
+                    total = value;
+                }
+                else if (column.opeator == '+')
+                {
+                    total += value;
+                }
+                else
+                {
+                    total *= value;
+                }
+            }
+            System.Console.WriteLine(total);
+            result += total;
+        }
 
 
         System.Console.WriteLine("Part 2 = {0}", result);
